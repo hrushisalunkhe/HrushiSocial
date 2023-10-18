9a858f33a27e9d847bd08239a4d9fbf7710afd95
hrushisalunkhe <salunkhehrushi1@gmail.com>
2023-10-18 20:32:27 +0530

    added another controller

---
+++ controller/users_controller.js
@@ -0,0 +1,4 @@
+module.exports.profile = function (req,res) {
+
+    res.end('<h1> this is user profile </h1>')
+}
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ console.log('router loaded');
 const homeController = require('../controller/Home_Controller');
 
 router.get ('/' , homeController.home);
+router.use('/users' , require('./users'))
 
 module.exports = router;
